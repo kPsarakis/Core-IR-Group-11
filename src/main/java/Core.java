edeb9ad80c007a62dafc39bc5858a2c911d63fee
agamvrinos <agg_gam@hotmail.com>
2019-03-07 02:31:46 +0100

    Update implementation
    - read input queries from files instead of manually
    - add | delimiter between prefix and candidate
    - save results to files to be used by lambdaMART

--- src/main/java/Core.java
+++ src/main/java/Core.java
@@ -1,9 +1,26 @@
-import java.util.*;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Core {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         final CorpusReader reader = new CorpusReader();     // initialize the reader
         final List<String> queries = reader.getQueries();   // get the read queries
@@ -13,38 +30,118 @@ public class Core {
 
 	    // Read user input (Simulate partial query typing)
 	    Scanner scanner = new Scanner(System.in);
-        String prefix = "";
-        Integer scenario = null;
+	    String filepath = "";
+	    String pattern = "yyyyMMddHHmm-";
+	    DateFormat df = new SimpleDateFormat(pattern);
 
-        while(!prefix.equals("exit")) {
-	        System.out.print("\nEnter query prefix: ");
-	        prefix = scanner.nextLine();
-	        System.out.println("\nGenerate candidates for scenario (1, 2, 3): ");
-	        scenario = Integer.parseInt(scanner.nextLine());
-	        if (!scenario.equals(1) && !scenario.equals(2) && !scenario.equals(3)) {
-		        System.out.println("man mou dwse swsto scenario tin epomeni");
-		        return;
+        while(true) {
+		    System.out.print("File to generate candidates for (1:train, 2:valid, 3:test): ");
+	        int chosenFile = Integer.parseInt(scanner.nextLine());
+		    System.out.print("Choose scenario (1, 2 or 3): ");
+		    int scenario = Integer.parseInt(scanner.nextLine());
+
+		    switch (chosenFile) {
+	            case 1:
+		            filepath = "src/main/java/data/training-queries.txt";
+		            break;
+		        case 2:
+		            filepath = "src/main/java/data/validation-queries.txt";
+			        break;
+		        case 3:
+			        filepath = "src/main/java/data/test-queries.txt";
+			        break;
 	        }
+	        List<String> userInputQueries;
 
-	        List<String> mergedCandidates = new ArrayList<>();
-	        mergedCandidates.addAll(processor.getFullQueryCandidates(prefix));  // full-query candidates are for all scenarios
+		    try {
+			    String now = df.format(Calendar.getInstance().getTime());
 
-	        List<String> syntheticQueryCandidates;
+			    BufferedWriter writer = new BufferedWriter(new FileWriter(now + "results_" + filepath.substring(filepath.lastIndexOf("/") + 1)));
 
-	        if (scenario.equals(2)) {
-	        	// Full-query based candidates + Suffix based candidates (top 10k)
-		        syntheticQueryCandidates = processor.getSyntheticQueryCandidates(prefix, 10000);
-		        mergedCandidates.addAll(syntheticQueryCandidates);
-	        } else if (scenario.equals(3)){
-		        // Full-query based candidates + Suffix based candidates (top 100k)
-		        syntheticQueryCandidates = processor.getSyntheticQueryCandidates(prefix, 100000);
-		        mergedCandidates.addAll(syntheticQueryCandidates);
-	        }
+			    Stream<String> lines = Files.lines(Paths.get(filepath));
+			    userInputQueries = lines.collect(Collectors.toList());
+			    lines.close();
 
-	        //TODO: To be used by LAMBDA MART i guess
-            Map<String, Integer> aggregatedCandidates = aggregateCandidates(mergedCandidates);
-	        System.out.println("yolo");
+			    for (String userInputQuery : userInputQueries) {
+			    	userInputQuery = "www ngoforum org tr";
+
+					String[] userInputQuerySplit = userInputQuery.split(" ", 2);
+					String firstWord = userInputQuerySplit[0]; // the first work as a whole
+
+				    if (userInputQuerySplit.length > 1) {
+				        // not a single word case
+
+					    String restOfQuery = " " + userInputQuerySplit[1];
+					    String currentQuery = firstWord;
+
+					    for (int i = 0; i < restOfQuery.length(); i++){
+						    List<String> mergedCandidates = new ArrayList<>();
+						    mergedCandidates.addAll(processor.getFullQueryCandidates(currentQuery));  // full-query candidates are for all scenarios
+
+						    List<String> syntheticQueryCandidates;
+
+						    if (scenario == 2) {
+							    // Full-query based candidates + Suffix based candidates (top 10k)
+							    syntheticQueryCandidates = processor.getSyntheticQueryCandidates(currentQuery, 10000);
+							    mergedCandidates.addAll(syntheticQueryCandidates);
+						    } else if (scenario == 3){
+							    // Full-query based candidates + Suffix based candidates (top 100k)
+							    syntheticQueryCandidates = processor.getSyntheticQueryCandidates(currentQuery, 100000);
+							    mergedCandidates.addAll(syntheticQueryCandidates);
+						    }
+
+						    Map<String, Integer> aggregatedCandidates = aggregateCandidates(mergedCandidates);
+
+						    for (Map.Entry<String, Integer> candidate : aggregatedCandidates.entrySet()) {
+						        String candidateWithoutDelim = candidate.getKey().replace("|", "");
+						        Integer relevanceJudgement = (candidateWithoutDelim.equals(userInputQuery)) ? 1 : 0;
+
+							    String line = candidate.getKey() + "\t" + relevanceJudgement.toString() + "\n";
+							    writer.write(line);
+						    }
+						    char currentChar = restOfQuery.charAt(i);
+						    currentQuery = currentQuery + currentChar;
+					    }
+					    writer.close();
+				    }
+
+				    //TODO: REMOVE ME
+				    break;
+			    }
+
+		    } catch(IOException io) {
+			    io.printStackTrace();
+		    }
         }
+    }
+
+    private static void test(CorpusProcessor processor, String prefix) {
+    	Scanner scanner = new Scanner(System.in);
+    	Integer scenario = 2;
+
+	    while(!prefix.equals("exit")) {
+		    System.out.print("\nEnter query prefix: ");
+		    prefix = scanner.nextLine();
+		    System.out.println("\nGenerate candidates for scenario (1, 2, 3): ");
+
+		    List<String> mergedCandidates = new ArrayList<>();
+		    mergedCandidates.addAll(processor.getFullQueryCandidates(prefix));  // full-query candidates are for all scenarios
+
+		    List<String> syntheticQueryCandidates;
+
+		    if (scenario.equals(2)) {
+			    // Full-query based candidates + Suffix based candidates (top 10k)
+			    syntheticQueryCandidates = processor.getSyntheticQueryCandidates(prefix, 10000);
+			    mergedCandidates.addAll(syntheticQueryCandidates);
+		    } else if (scenario.equals(3)){
+			    // Full-query based candidates + Suffix based candidates (top 100k)
+			    syntheticQueryCandidates = processor.getSyntheticQueryCandidates(prefix, 100000);
+			    mergedCandidates.addAll(syntheticQueryCandidates);
+		    }
+
+		    //TODO: To be used by LAMBDA MART i guess
+		    Map<String, Integer> aggregatedCandidates = aggregateCandidates(mergedCandidates);
+	    }
     }
 
     private static Map<String, Integer> aggregateCandidates(List<String> mergedCandidates) {
